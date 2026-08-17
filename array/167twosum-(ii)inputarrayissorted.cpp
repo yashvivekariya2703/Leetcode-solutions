d3d8d46f7e23ecdd7a81e5eed45a428ca15b6303
Yashvi Vekariya <yashvivekariya2703@gmail.com>
2026-08-17 23:38:02 +0530

    "Solved binary search and two pointer problems"

---
+++ array/167twosum-(ii)inputarrayissorted.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int start=0,end=numbers.size()-1;
+        for(int i=start;i<numbers.size()-1;i++){
+        while(start<end)
+        {
+            
+            if(numbers[start]+numbers[end]==target)
+            {
+                break;
+            }
+            else if(numbers[start]+numbers[end]>target)
+            {
+                end--;
+            }
+            else{
+                start++;
+            }
+            }
+        }
+        return{start+1,end+1};
+    }
+};
