16650a8c6919237f5daadfc4220da17e32ac37be
Yashvi Vekariya <yashvivekariya2703@gmail.com>
2026-08-20 23:02:15 +0530

    solved problem of sorting

---
+++ 1051.HeightChecker.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int>expected=heights;
+        int n=heights.size();
+        sort(expected.begin(),expected.end());
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(heights[i]!=expected[i])
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
